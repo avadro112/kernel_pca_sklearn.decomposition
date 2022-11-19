58e861ef52f5bcfc4023ad170b7553fdae94a761
avadro112 <jkanant@gmail.com>
2022-11-19 15:20:51 +0530

    pirnting accuracy and confusion matrix

--- model.py
+++ model.py
@@ -14,3 +14,16 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.fit_transform(x_test)
+
+from sklearn.decomposition import KernelPCA
+kernel = KernelPCA(n_components=2,kernel='rbf')
+x_train = kernel.fit_transform(x_train)
+x_test = kernel.fit_transform(x_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+y_pred = classifier.fit(x_train,y_train).predict(x_test)
+
+from sklearn.metrics import accuracy_score,confusion_matrix
+print(confusion_matrix(y_test,y_pred))
+print(accuracy_score(y_test,y_pred))
