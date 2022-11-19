57f0a9fed42e03a16f9f0a50f30a4aab960099e2
avadro112 <jkanant@gmail.com>
2022-11-19 15:15:20 +0530

    importing StandardScaler

--- model.py
+++ model.py
@@ -8,3 +8,9 @@ y = dataset.iloc[:,-1].values
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test= train_test_split(x,y,random_state=0,test_size=0.2)
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
