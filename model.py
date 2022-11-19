2e3429448e95b1c1db6325d7579efac85a199e09
avadro112 <jkanant@gmail.com>
2022-11-19 15:11:25 +0530

    spliting dataset to train and test

--- model.py
+++ model.py
@@ -3,3 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 dataset = pd.read('file.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test= train_test_split(x,y,random_state=0,test_size=0.2)
